822b8612b6c61d76701b08d82f0a6474a47b6fbb
andr3sc0des <andresqcodes@gmail.com>
2023-01-23 14:36:41 -0600

    initial commit

---
+++ pages/Inputs.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Buttons.module.sass'
+const Inputs = () => {
+  return (
+    <>
+      <section className={styles.buttons}>
+        <h1 className={styles.buttons__title}>Inputs</h1>
+      </section>
+    </>
+  )
+}
+
+export default Inputs
---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home () {
+  return (
+    <>
+      <Head>
+        <title>Button Components</title>
+        <meta name='description' content='Created by andr3sc0des' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='./devchallenges.png' />
+        <link rel='shortcut icon' type='image/x-icon' href='https://devchallenges.io/' />
+        <meta http-equiv='refresh' content="0; URL='http://127.0.0.1:3000/Buttons'" />
+      </Head>
+      <main />
+    </>
+  )
+}
