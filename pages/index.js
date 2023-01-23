ea6642fa474bf8b35a4c030d1fc9e6faf68cb03c
andr3sc0des <andresqcodes@gmail.com>
2023-01-23 14:44:22 -0600

    redirection fix

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home () {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='./devchallenges.png' />
         <link rel='shortcut icon' type='image/x-icon' href='https://devchallenges.io/' />
-        <meta http-equiv='refresh' content="0; URL='http://127.0.0.1:3000/Buttons'" />
+        <meta http-equiv='refresh' content="0; URL='https://andr3sc0des.github.io/button-component/Buttons'" />
       </Head>
       <main />
     </>
