20941783debaadee823f796fe5fe04c642e4d1aa
andr3sc0des <andresqcodes@gmail.com>
2023-01-23 14:50:39 -0600

    redirection fix

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Buttons from './Buttons'
 
 export default function Home () {
   return (
@@ -7,11 +8,12 @@ export default function Home () {
         <title>Button Components</title>
         <meta name='description' content='Created by andr3sc0des' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='./devchallenges.png' />
+        <link rel='icon' href='devchallenges.png' />
         <link rel='shortcut icon' type='image/x-icon' href='https://devchallenges.io/' />
-        <meta http-equiv='refresh' content="0; URL='https://andr3sc0des.github.io/button-component/Buttons'" />
       </Head>
-      <main />
+      <main>
+        <Buttons />
+      </main>
     </>
   )
 }
